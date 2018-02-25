98a8de5d41cec0530aaf5555a00c413c4a873985
CrashOverride <Cr4shOverride@gmx.de>
2018-02-25 16:33:50 +0100

    Create BTC_advanced_test.cpp

---
+++ BTC_advanced_test.cpp
@@ -0,0 +1,25 @@
+//created by Cr4shOv3rrid3 under the 1337 General Public BTC Banking License 
+// feel free to copy, hack, destroy or create new things with it
+// P.S. if you don't like i created that for you. just replace first line with your own cool stylish hacker name ;)
+
+#include <iostream>
+#include <stdio.h>
+#include <BTC_advanced_test.h>  //if it won't compile copy everything from it and place it before main(... and comment out this line
+
+using namespace std;
+
+int main()
+{
+
+settings();
+testprint();
+
+int count = 10;
+char ch;
+while(cin.get(ch) != ".")
+count++;
+cout << "Numbers of characters read: \n" << count;
+cout << "Test print!!!";
+
+        return 0;
+} //Basic Advanced Bitcoin generator Test Programmm in 25 Lines of code
